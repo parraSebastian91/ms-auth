b43e4d12c152a9cfb83b97670ddbb5c2644b6cf9
sebastian <parra.sebastian91@gmail.com>
2025-10-04 13:10:07 -0300

    cors policy and update docker-compose

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from './infrastructure/http-server/pipes/validation.pi
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+    // Deshabilitar CORS completamente
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000).then(() => {
     console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
   });
